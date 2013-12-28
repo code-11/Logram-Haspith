c4a7e50053e8aa56d69908e4ea66187f2010c6a2
Brendan Ritter <brendan.ritter@yahoo.com>
2013-12-28 00:12:01 -0500

    Made a mock-up of the battle scene
    The Lincoln sprite happened to be the first one I found.

---
+++ Assets/Fight_Menu.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Fight_Menu : MonoBehaviour
+{
+	
+		void OnGUI ()
+		{
+				// Make a background box
+				GUI.Box (new Rect (10, 10, 130, 140), "Action Menu");
+		
+				if (GUI.Button (new Rect (20, 40, 110, 20), "Physical Attack")) {
+						Debug.Log ("Physical Attack");
+				}
+				if (GUI.Button (new Rect (20, 65, 110, 20), "Magic Attack")) {
+						Debug.Log ("Magic Attack");
+				}
+				if (GUI.Button (new Rect (20, 90, 110, 20), "Wait/Defend")) {
+						Debug.Log ("Wait/Defend");
+				}
+				if (GUI.Button (new Rect (20, 115, 110, 20), "Withdraw")) {
+						Debug.Log ("Withdraw");
+				}
+		}
+}
